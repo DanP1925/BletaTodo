c3fe25bf9f04945ffa8face798cda748d0483718
Daniel Peñaloza <DanP1925@gmail.com>
2024-10-08 02:04:34 +0200

    Change how to initialize TaskScreenState

--- app/src/main/java/com/danp1925/todolist/presentation/tasklist/TasksViewModel.kt
+++ app/src/main/java/com/danp1925/todolist/presentation/tasklist/TasksViewModel.kt
@@ -6,7 +6,11 @@ import com.danp1925.todolist.domain.usecases.GetTasksUseCase
 import com.danp1925.todolist.presentation.tasklist.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,21 +20,13 @@ class TasksViewModel @Inject constructor(
     private val getTasksUseCase: GetTasksUseCase,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(TasksScreenState())
-    val uiState: StateFlow<TasksScreenState> = _uiState
-
-    init {
-        loadTasks()
-    }
-
-    private fun loadTasks() {
-        viewModelScope.launch {
-            getTasksUseCase().collect { tasks ->
-                _uiState.update {
-                    _uiState.value.copy(tasks = tasks.map { it.toUI() })
-                }
-            }
-        }
-    }
+    private val _tasks = getTasksUseCase().distinctUntilChanged()
+    val uiState: StateFlow<TasksScreenState> = combine(_tasks) { tasks ->
+        TasksScreenState(tasks.first().map { it.toUI() })
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = TasksScreenState()
+    )
 
 }
